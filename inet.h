d18c7f19a6a713031821d50d62ff30a2c444e71e
the-reds-ynwa <narishakamarudin@gmail.com>
2015-09-30 11:18:10 +0800

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,21 @@
+/*	inet.h file
+
+	TTP3121 TCP/IP Programming Project (Trimester 1, 2015/16)
+	Members:    1) Praweens 1131120891
+              2) Narisha 1122702438
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
